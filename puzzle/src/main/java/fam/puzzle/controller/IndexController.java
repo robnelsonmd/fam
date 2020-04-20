339c7792b4813453ff25c3ac1961c6c94f687455
Rob Nelson <rob.nelson.md@gmail.com>
2020-04-20 15:48:15 -0400

    Add logout option.

--- puzzle/src/main/java/fam/puzzle/controller/IndexController.java
+++ puzzle/src/main/java/fam/puzzle/controller/IndexController.java
@@ -6,10 +6,15 @@ import fam.puzzle.service.PlayerService;
 import fam.puzzle.service.PuzzleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
@@ -84,6 +89,17 @@ public class IndexController extends AbstractController {
         generateNewPuzzle(session);
 
         return "index";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:index";
     }
 
     private void generateNewPuzzle(HttpSession session) {
