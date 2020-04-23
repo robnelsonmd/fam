3888cf9e3f7ac30bd84708ab82d6d3f7284ab497
Rob Nelson <rob.nelson.md@gmail.com>
2020-04-22 21:20:25 -0400

    Don't pre-generate puzzles during development.

--- puzzle/src/main/java/fam/puzzle/service/PuzzleServiceImpl.java
+++ puzzle/src/main/java/fam/puzzle/service/PuzzleServiceImpl.java
@@ -4,10 +4,12 @@ import fam.puzzle.domain.Puzzle;
 import fam.puzzle.generator.PuzzleGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
@@ -20,11 +22,13 @@ public class PuzzleServiceImpl implements PuzzleService {
 
     private final List<Puzzle> fourDigitPuzzleCache = new CopyOnWriteArrayList<>();
     private final List<Puzzle> threeDigitPuzzleCache = new CopyOnWriteArrayList<>();
+    private final Environment environment;
     private final PuzzleGenerator puzzleGenerator;
     private final ScheduledExecutorService threeDigitPuzzleGeneratorService = Executors.newSingleThreadScheduledExecutor();
     private final ScheduledExecutorService fourDigitPuzzleGeneratorService = Executors.newScheduledThreadPool(3);
 
-    public PuzzleServiceImpl(PuzzleGenerator puzzleGenerator) {
+    public PuzzleServiceImpl(Environment environment, PuzzleGenerator puzzleGenerator) {
+        this.environment = environment;
         this.puzzleGenerator = puzzleGenerator;
     }
 
@@ -35,8 +39,11 @@ public class PuzzleServiceImpl implements PuzzleService {
 
     @PostConstruct
     public void initializePuzzleGenerators() {
-        threeDigitPuzzleGeneratorService.scheduleWithFixedDelay(this::generateThreeDigitPuzzles, 0, 1, TimeUnit.SECONDS);
-        fourDigitPuzzleGeneratorService.scheduleWithFixedDelay(this::generateFourDigitPuzzles, 0, 1, TimeUnit.SECONDS);
+        boolean devProfileActive = Arrays.asList(environment.getActiveProfiles()).contains("dev");
+        if (!devProfileActive) {
+            threeDigitPuzzleGeneratorService.scheduleWithFixedDelay(this::generateThreeDigitPuzzles, 0, 1, TimeUnit.SECONDS);
+            fourDigitPuzzleGeneratorService.scheduleWithFixedDelay(this::generateFourDigitPuzzles, 0, 1, TimeUnit.SECONDS);
+        }
     }
 
     @PreDestroy
